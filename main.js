068f4eeba98c8327db53d0eeb176e1aa578d06ca
Lee Barton <hleebarton97@gmail.com>
2018-03-09 13:38:05 -0600

    Auto updates

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const electron = require('electron')
 const {ipcMain} = require('electron');
-const {autoUpdater} = require("electron-updater");
+const {autoUpdater} = require('electron-updater');
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
-const path = require('path')
+const path = reappquire('path')
 const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
